bcab8c2c654a2776c4fc11366325a56c5e82058b
IEMELIANOV <emelianov@webmedved.ru>
2025-01-29 13:21:14 +0500

    fix

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'normalize.css';
+import './index.scss';
+import './fonts/fonts.scss';
+import App from './components/app/app';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+reportWebVitals();
